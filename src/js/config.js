baf317f6557f9e2c669d9680b5bbe99a795c7cb4
Gustavo <alf4r6@gmail.com>
2022-10-05 00:39:21 -0300

    backup

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+const TOKEN = "514f81dbe2d693bf586d2f5d9d119b24"
+export default TOKEN 
+
+const apiKey = "fa0b00c6bcddb8f5b0dcb2e764185060"
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
-const ipBtn = document.querySelector(".get-btn")
+const ipBtn = document.getElementById("ipBtn")
 const ipDisplay = document.querySelector(".ip-display")
 const ipv6Display = document.querySelector(".ipv6-display")
 const locationDisplay = document.querySelector(".location-display")
 
+import TOKEN from "./config.js" 
 
 
 
@@ -13,7 +14,7 @@ ipBtn.addEventListener("click", () => {
     console.log(response)
   }) 
    
-  axios.get("http://api.ipapi.com/api/check?access_key=514f81dbe2d693bf586d2f5d9d119b24").then(ipv6 => {
+  axios.get(`http://api.ipapi.com/api/check?access_key=${TOKEN}`).then(ipv6 => {
     ipv6Display.textContent = `IPv6 Address : ${ipv6.data.ip}`   
     console.log(ipv6)
   })
@@ -23,4 +24,22 @@ ipBtn.addEventListener("click", () => {
     console.log.log(locationIp)
   })
 
+  axios.get(`http://api.ipapi.com/api/check?access_key=${TOKEN}`).then(loglat => {
+    logLat.push()
+  }) 
+
 })
+
+ let logLat = [
+
+ ]
+
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
